b61fd1f3d4a9e709204ca1f94a595b7d65da7edc
Alexandre Ogrodovski <2933195+aleogr@users.noreply.github.com>
2026-06-30 14:39:05 -0300

    playground/goapp initial commit

---
+++ playground/goapp/cmd/app1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//indent := ""
+	fmt.Println("app1 -> main()")
+
+	// Iniciamos o trace com um nível de indentação
+	//prvpkg1.Hello(indent + "  ")
+	//prvlib1.Hello(indent + "  ")
+	//publib1.Hello(indent + "  ")
+}
